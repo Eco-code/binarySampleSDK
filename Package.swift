5fa9138e489fb731aae8c645ce1121131d7840ed
Eco-code <elestevy@gmail.com>
2023-09-08 12:19:13 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "binarySampleSDK",
+    name: "NTransitFramework",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "binarySampleSDK",
-            targets: ["binarySampleSDK"]),
+            name: "NTransitFramework",
+            targets: ["NTransitFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
